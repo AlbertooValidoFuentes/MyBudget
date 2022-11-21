898b533c4e91d5dbb4cb60984397928950bd21f9
Alberto <albertovalidofuentes2001@gmail.com>
2022-11-21 22:02:41 +0000

    Refactor DataInput component to DataInputIncome

--- App.jsx
+++ App.jsx
@@ -2,7 +2,7 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 import Balance from './components/Balance';
-import DataInput from './components/DataInput';
+import DataInput from './components/DataInputIncome';
 import ListItem from './components/ListItem';
 import OptionsButtons from './components/OptionsButtons';
 
