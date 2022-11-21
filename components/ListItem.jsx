a8cf13ec17e00529e101df0bea617d5dfaeca244
Alberto <albertovalidofuentes2001@gmail.com>
2022-11-21 21:03:40 +0000

    Add ListItem component

---
+++ components/ListItem.jsx
@@ -0,0 +1,13 @@
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function ListItem() {
+  return (
+      <View>
+        <Text>Item</Text>
+      </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  
+});
