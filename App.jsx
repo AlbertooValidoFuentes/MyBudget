98832a707e89e9d8e24e0ab41f8df2ebaad100fe
Alberto <albertovalidofuentes2001@gmail.com>
2022-11-21 20:53:56 +0000

    Add states

--- App.jsx
+++ App.jsx
@@ -1,13 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
 
 import Balance from './components/Balance';
 import OptionsButtons from './components/OptionsButtons';
 
 export default function App() {
+
+  const [balance, setBalance] = useState(0)
+  const [transaction, setTransaction] = useState([])
+
   return (
     <View style={styles.container}>
-      <Balance/>
+      <Balance balance={balance}/>
       <OptionsButtons/>
       <StatusBar style="auto" hidden={true}/>
     </View>
