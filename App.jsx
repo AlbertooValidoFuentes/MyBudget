08862e52432b0b2f05c3d89bdacfe3ab2e4895d3
Alberto <albertovalidofuentes2001@gmail.com>
2022-11-22 20:13:05 +0000

    Add remove item

--- App.jsx
+++ App.jsx
@@ -13,21 +13,25 @@ export default function App() {
   const [balance, setBalance] = useState(0)
   const [transaction, setTransaction] = useState([])
 
-  console.log(transaction);
-  
   return (
     <View style={styles.container}>
-      <Balance balance={balance}/>
-      <OptionsButtons setTransaction={setTransaction} transaction={transaction} setBalance={setBalance} balance={balance}/>
+      <Balance balance={balance} />
+      <OptionsButtons setTransaction={setTransaction} transaction={transaction} setBalance={setBalance} balance={balance} />
 
-      <FlatList data={transaction}  style={styles.list} renderItem={(renderItem) => {
-        const {tipo, cantidad, descripcion, fecha} = renderItem.item
-        return(
-          <ListItem tipo={tipo} cantidad={cantidad} descripcion={descripcion} fecha={fecha}/>
+      <FlatList data={transaction} style={styles.list} renderItem={(renderItem) => {
+        const { id, tipo, cantidad, descripcion, fecha } = renderItem.item
+        return (
+          <ListItem
+            tipo={tipo}
+            cantidad={cantidad}
+            descripcion={descripcion}
+            fecha={fecha} id={id}
+            setTransaction={setTransaction}
+            transaction={transaction} />
         )
       }}
       />
-      <StatusBar style="auto" hidden={true}/>
+      <StatusBar style="auto" hidden={true} />
     </View>
   );
 }
--- components/ListItem.jsx
+++ components/ListItem.jsx
@@ -1,13 +1,29 @@
-import { FlatList, Pressable, StyleSheet, Text, View } from 'react-native';
+import { FlatList, Pressable, StyleSheet, Text, View, Image } from 'react-native';
 
-export default function ListItem({tipo, cantidad, descripcion, fecha }) {
+export default function ListItem({ tipo, cantidad, descripcion, fecha, id, setTransaction, transaction }) {
+
+  const removeProductHandler = () => {
+    setTransaction(() => transaction.filter((transaction) => transaction.id !== id))
+  }
+
   return (
-      <Pressable style={styles.itemContainer}>
-        <Text style={styles.itemText}>Tipo: {tipo}.</Text>
-        <Text style={styles.itemText}>Cantidad: {cantidad}€</Text>
-        <Text style={styles.itemText}>Descripcion: {descripcion}.</Text>
-        <Text style={styles.itemText}>Fecha: {fecha}</Text>
-      </Pressable>
+    <Pressable style={styles.itemContainer}>
+      <Text style={styles.itemText}>Tipo: {tipo}.</Text>
+      <Text style={styles.itemText}>Cantidad: {cantidad}€</Text>
+      <Text style={styles.itemText}>Descripcion: {descripcion}.</Text>
+      <Text style={styles.itemText}>Fecha: {fecha}</Text>
+
+      <View style={styles.itemOptions}>
+        <Pressable onPress={removeProductHandler}>
+          <Image source={require('../assets/eliminar.png')} style={styles.optionsImage} />
+        </Pressable>
+        <Pressable>
+          <Image source={require('../assets/lapiz.png')} style={styles.optionsImage} />
+        </Pressable>
+      </View >
+    </Pressable>
+
+
   );
 }
 
@@ -25,5 +41,14 @@ const styles = StyleSheet.create({
     fontSize: 16,
     fontWeight: 'bold'
 
+  },
+  itemOptions: {
+    flexDirection: 'row',
+    marginTop: 10,
+  },
+  optionsImage: {
+    width: 25,
+    height: 25,
+    margin: 5
   }
 });
